365268aed3df4a182c51605a036639c8e6e3cc66
Adeshina Adam <adeshinaadam03@gmail.com>
2025-06-15 16:05:09 -0700

    Update projects.tsx

--- components/projects.tsx
+++ components/projects.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { Badge } from "@/components/ui/badge"
 import { ExternalLink, Github } from "lucide-react"
-import {Link} from 'next/link'
+import Link from 'next/link'
 
 export function Projects() {
   const projects = [
